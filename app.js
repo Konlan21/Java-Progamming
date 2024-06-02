c4c3e062faab6f96c60d03a22a695e57c2d9c9be
Konlan21 <joekonlan16@gmail.com>
2024-06-02 19:04:15 +0000

    Add app.js to ignore file

--- app.js
+++ app.js
@@ -1,2 +1,17 @@
-app.py
-app.js
+
+class Comparable {
+    constructor(points, user) {
+        this.user = user;
+        this.points = points;
+    }
+
+    compare(user) {
+        user.points = other.points;
+    }
+}
+
+
+let user1 = new Comparable(23);
+let user2 = new Comparable(34);
+
+console.log(user1 == user2);
